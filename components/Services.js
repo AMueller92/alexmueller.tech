302eb05e44c67c1d198c8c6b31321a35db2a0698
Alexander <alexandermueller492@gmail.com>
2021-07-02 09:42:32 +0200

    fix grid and add to testimonials

--- components/Services.js
+++ components/Services.js
@@ -6,14 +6,11 @@ import {
   Card,
   Heading,
   Paragraph,
-  Grid,
-  ResponsiveContext,
 } from "grommet";
+import ResponsiveGrid from "@components/utils/ResponsiveGrid";
 
 // If the size is small, we only see 1 column
-// If the size is medium, we only see 2 columns
-// If the size is either large or xlarge, we see 3 columns
-
+// If the size is medium or above, we see 2 columns
 const columns = {
   small: ["auto"],
   medium: ["auto", "auto"],
@@ -21,12 +18,19 @@ const columns = {
   xlarge: ["auto", "auto"],
 };
 
-// If the size is small, we have 3 rows
-// If the size is medium, we have 2 rows
-// If the size is large or xlarge, we have 1 row
-
+// If the size is small, we have 8 rows
+// If the size is medium or above, we have 2 rows
 const rows = {
-  small: ["xsmall", "xsmall", "xsmall"],
+  small: [
+    "xsmall",
+    "xsmall",
+    "xsmall",
+    "xsmall",
+    "xsmall",
+    "xsmall",
+    "xsmall",
+    "xsmall",
+  ],
   medium: ["small", "small"],
   large: ["small", "small"],
   xlarge: ["small", "small"],
@@ -36,159 +40,109 @@ const rows = {
 
 const fixedGridAreas = {
   small: [
-    { name: "header", start: [0, 0], end: [0, 0] },
-    { name: "test", start: [0, 1], end: [0, 1] },
-    { name: "test1", start: [0, 2], end: [0, 2] },
+    { name: "service1", start: [0, 0], end: [0, 1] },
+    { name: "service2", start: [0, 2], end: [0, 3] },
+    { name: "service3", start: [0, 4], end: [0, 5] },
+    { name: "service4", start: [0, 6], end: [0, 7] },
   ],
   medium: [
-    { name: "header", start: [0, 0], end: [1, 0] },
-    { name: "test", start: [0, 1], end: [0, 1] },
-    { name: "test1", start: [1, 1], end: [1, 1] },
+    { name: "service1", start: [0, 0], end: [0, 0] },
+    { name: "service2", start: [1, 0], end: [1, 0] },
+    { name: "service3", start: [0, 1], end: [0, 1] },
+    { name: "service4", start: [1, 1], end: [1, 1] },
   ],
   large: [
-    { name: "header", start: [0, 0], end: [1, 0] },
-    { name: "test", start: [0, 1], end: [0, 1] },
-    { name: "test1", start: [1, 1], end: [1, 1] },
+    { name: "service1", start: [0, 0], end: [0, 0] },
+    { name: "service2", start: [1, 0], end: [1, 0] },
+    { name: "service3", start: [0, 1], end: [0, 1] },
+    { name: "service4", start: [1, 1], end: [1, 1] },
   ],
   xlarge: [
-    { name: "header", start: [0, 0], end: [1, 0] },
-    { name: "test", start: [0, 1], end: [0, 1] },
-    { name: "test1", start: [1, 1], end: [1, 1] },
+    { name: "service1", start: [0, 0], end: [0, 0] },
+    { name: "service2", start: [1, 0], end: [1, 0] },
+    { name: "service3", start: [0, 1], end: [0, 1] },
+    { name: "service4", start: [1, 1], end: [1, 1] },
   ],
 };
-
-const Responsive = ({
-  children,
-  overrideColumns,
-  overrideRows,
-  areas,
-  ...props
-}) => (
-  <ResponsiveContext.Consumer>
-    {(size) => {
-      // Take into consideration if not array is sent but a simple string
-      let columnsVal = columns;
-      if (columns) {
-        if (columns[size]) {
-          columnsVal = columns[size];
-        }
-      }
-
-      let rowsVal = rows;
-      if (rows) {
-        if (rows[size]) {
-          rowsVal = rows[size];
-        }
-      }
-
-      // Also if areas is a simple array not an object of arrays for
-      // different sizes
-      let areasVal = areas;
-      if (areas && !Array.isArray(areas)) areasVal = areas[size];
-
-      return (
-        <Grid
-          {...props}
-          areas={!areasVal ? undefined : areasVal}
-          rows={!rowsVal ? size : rowsVal}
-          columns={!columnsVal ? size : columnsVal}
-        >
-          {children}
-        </Grid>
-      );
-    }}
-  </ResponsiveContext.Consumer>
-);
 
 export default function Services() {
   return (
     <Box>
-      <Text
-        size="4xl"
-        textAlign="center"
-        color="light-1"
-        style={{ marginBottom: 40 }}
-      >
+      <Text size="4xl" textAlign="center" color="light-1">
         Services
       </Text>
-      <Responsive
+      <ResponsiveGrid
         rows={rows}
         columns={columns}
-        gap="small"
+        gap="medium"
         areas={fixedGridAreas}
-        margin="medium"
+        margin="large"
       >
-        <Card
-          pad="medium"
-          width="medium"
-          gap="medium"
-          background="light-1"
-          gridArea="header"
-        >
-          <CardHeader>
-            <Heading level={3} margin="none">
-              Service 1
-            </Heading>
-          </CardHeader>
-          <CardBody>
-            <Paragraph margin="none">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            </Paragraph>
-          </CardBody>
-        </Card>
-        <Card
-          pad="medium"
-          width="medium"
-          gap="medium"
-          background="light-1"
-          gridArea="test"
-        >
-          <CardHeader>
-            <Heading level={3} margin="none">
-              Service 2
-            </Heading>
-          </CardHeader>
-          <CardBody>
-            <Paragraph margin="none">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            </Paragraph>
-          </CardBody>
-        </Card>
-        <Card
-          pad="medium"
-          width="medium"
-          gap="medium"
-          background="light-1"
-          gridArea="test1"
-        >
-          <CardHeader>
-            <Heading level={3} margin="none">
-              Service 3
-            </Heading>
-          </CardHeader>
-          <CardBody>
-            <Paragraph margin="none">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            </Paragraph>
-          </CardBody>
-        </Card>
-        {/*<Card pad="medium" width="medium" gap="medium" background="light-1">*/}
-        {/*  <CardHeader>*/}
-        {/*    <Heading level={3} margin="none">*/}
-        {/*      Service 4*/}
-        {/*    </Heading>*/}
-        {/*  </CardHeader>*/}
-        {/*  <CardBody>*/}
-        {/*    <Paragraph margin="none">*/}
-        {/*      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam*/}
-        {/*      nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam*/}
-        {/*    </Paragraph>*/}
-        {/*  </CardBody>*/}
-        {/*</Card>*/}
-      </Responsive>
+        <Box gridArea="service1" justify="center" align="center">
+          <Card pad="medium" width="large" gap="medium" background="light-1">
+            <CardHeader>
+              <Heading level={3} margin="none">
+                Service 1
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Paragraph margin="none">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam
+              </Paragraph>
+            </CardBody>
+          </Card>
+        </Box>
+        <Box gridArea="service2" justify="center" align="center">
+          <Card pad="medium" width="large" gap="medium" background="light-1">
+            <CardHeader>
+              <Heading level={3} margin="none">
+                Service 2
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Paragraph margin="none">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam
+              </Paragraph>
+            </CardBody>
+          </Card>
+        </Box>
+        <Box gridArea="service3" justify="center" align="center">
+          <Card pad="medium" width="large" gap="medium" background="light-1">
+            <CardHeader>
+              <Heading level={3} margin="none">
+                Service 3
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Paragraph margin="none">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam
+              </Paragraph>
+            </CardBody>
+          </Card>
+        </Box>
+        <Box gridArea="service4" justify="center" align="center">
+          <Card pad="medium" width="large" gap="medium" background="light-1">
+            <CardHeader>
+              <Heading level={3} margin="none">
+                Service 4
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Paragraph margin="none">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam
+              </Paragraph>
+            </CardBody>
+          </Card>
+        </Box>
+      </ResponsiveGrid>
     </Box>
   );
 }
--- components/Testimonials.js
+++ components/Testimonials.js
@@ -1,7 +1,6 @@
 import {
   Card,
   CardBody,
-  CardFooter,
   CardHeader,
   Grid,
   Text,
@@ -9,60 +8,112 @@ import {
   Heading,
   Paragraph,
 } from "grommet";
+import ResponsiveGrid from "@components/utils/ResponsiveGrid";
+
+const columns = {
+  small: ["auto"],
+  medium: ["auto", "auto", "auto"],
+  large: ["auto", "auto", "auto"],
+  xlarge: ["auto", "auto", "auto"],
+};
+
+const rows = {
+  small: ["xsmall", "xsmall", "xsmall", "xsmall", "xsmall", "xsmall"],
+  medium: ["medium"],
+  large: ["medium"],
+  xlarge: ["medium"],
+};
+
+// Set the different areas you need for every size
+
+const fixedGridAreas = {
+  small: [
+    { name: "company1", start: [0, 0], end: [0, 1] },
+    { name: "company2", start: [0, 2], end: [0, 3] },
+    { name: "company3", start: [0, 4], end: [0, 5] },
+  ],
+  medium: [
+    { name: "company1", start: [0, 0], end: [0, 0] },
+    { name: "company2", start: [1, 0], end: [1, 0] },
+    { name: "company3", start: [2, 0], end: [2, 0] },
+  ],
+  large: [
+    { name: "company1", start: [0, 0], end: [0, 0] },
+    { name: "company2", start: [1, 0], end: [1, 0] },
+    { name: "company3", start: [2, 0], end: [2, 0] },
+  ],
+  xlarge: [
+    { name: "company1", start: [0, 0], end: [0, 0] },
+    { name: "company2", start: [1, 0], end: [1, 0] },
+    { name: "company3", start: [2, 0], end: [2, 0] },
+  ],
+};
 
 export default function Testimonials() {
   return (
     <Box>
-      <Text size="4xl" margin="large" textAlign="center">
-        Customers
+      <Text size="4xl" textAlign="center" style={{ marginTop: 50 }}>
+        What Customers are saying
       </Text>
-      <Grid gap="large" columns={{ count: "fit", size: "small" }}>
-        <Card pad="medium" width="medium" gap="medium" background="light-1">
-          <CardHeader>
-            <Heading level={3} margin="none">
-              Company A
-            </Heading>
-          </CardHeader>
-          <CardBody>
-            <Paragraph margin="none">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam voluptua. At vero eos et accusam et justo duo
-              dolores et ea rebum. Stet clita kasd gubergren
-            </Paragraph>
-          </CardBody>
-        </Card>
-        <Card pad="medium" width="medium" gap="medium" background="light-1">
-          <CardHeader>
-            <Heading level={3} margin="none">
-              Company B
-            </Heading>
-          </CardHeader>
-          <CardBody>
-            <Paragraph margin="none">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam voluptua. At vero eos et accusam et justo duo
-              dolores et ea rebum. Stet clita kasd gubergren
-            </Paragraph>
-          </CardBody>
-        </Card>
-        <Card pad="medium" width="medium" gap="medium" background="light-1">
-          <CardHeader>
-            <Heading level={3} margin="none">
-              Company C
-            </Heading>
-          </CardHeader>
-          <CardBody>
-            <Paragraph margin="none">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam voluptua. At vero eos et accusam et justo duo
-              dolores et ea rebum. Stet clita kasd gubergren
-            </Paragraph>
-          </CardBody>
-        </Card>
-      </Grid>
+      <ResponsiveGrid
+        rows={rows}
+        columns={columns}
+        gap="medium"
+        areas={fixedGridAreas}
+        margin="medium"
+      >
+        <Box gridArea="company1" justify="center" align="center">
+          <Card pad="medium" width="medium" gap="medium" background="light-1">
+            <CardHeader>
+              <Heading level={3} margin="none">
+                Company A
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Paragraph margin="none">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren
+              </Paragraph>
+            </CardBody>
+          </Card>
+        </Box>
+        <Box gridArea="company2" justify="center" align="center">
+          <Card pad="medium" width="medium" gap="medium" background="light-1">
+            <CardHeader>
+              <Heading level={3} margin="none">
+                Company B
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Paragraph margin="none">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren
+              </Paragraph>
+            </CardBody>
+          </Card>
+        </Box>
+        <Box gridArea="company3" justify="center" align="center">
+          <Card pad="medium" width="medium" gap="medium" background="light-1">
+            <CardHeader>
+              <Heading level={3} margin="none">
+                Company C
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Paragraph margin="none">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren
+              </Paragraph>
+            </CardBody>
+          </Card>
+        </Box>
+      </ResponsiveGrid>
     </Box>
   );
 }
